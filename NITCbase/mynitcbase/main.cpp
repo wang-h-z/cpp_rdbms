5b787e65428a8956deada4001ea551fd2f20c72e
wang-h-z <haozhenwang297@gmail.com>
2026-04-13 16:16:24 +0800

    trying out disk class

--- NITCbase/mynitcbase/main.cpp
+++ NITCbase/mynitcbase/main.cpp
@@ -2,6 +2,7 @@
 #include "Cache/OpenRelTable.h"
 #include "Disk_Class/Disk.h"
 #include "FrontendInterface/FrontendInterface.h"
+#include <iostream>
 
 int main(int argc, char *argv[]) {
   /* Initialize the Run Copy of Disk */
@@ -9,5 +10,18 @@ int main(int argc, char *argv[]) {
   // StaticBuffer buffer;
   // OpenRelTable cache;
 
-  return FrontendInterface::handleFrontend(argc, argv);
-}
+  unsigned char buffer[BLOCK_SIZE];
+  Disk::readBlock(buffer, 7000);
+  char message[] = "hello";
+  memcpy(buffer + 20, message, 6);
+  Disk::writeBlock(buffer, 7000);
+
+  unsigned char buffer2[BLOCK_SIZE];
+  char message2[6];
+  Disk::readBlock(buffer2, 7000);
+  memcpy(message2, buffer2 + 20, 6);
+  std::cout << message2 << std::endl;
+
+  return 0;
+  // return FrontendInterface::handleFrontend(argc, argv);
+} 
--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # C++ RDBMS
 
-
 This project is an ongoing Relational Database Management System (RDBMS) implementation project with the help of [NITCbase](https://nitcbase.github.io/) roadmap as an implementation guide.
 
