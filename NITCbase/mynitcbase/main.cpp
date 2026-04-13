048c3d950f5750414f3cd468f97887a5ee5fc8d8
wang-h-z <haozhenwang297@gmail.com>
2026-04-13 14:49:08 +0800

    add NITCbase setup

---
+++ NITCbase/mynitcbase/main.cpp
@@ -0,0 +1,13 @@
+#include "Buffer/StaticBuffer.h"
+#include "Cache/OpenRelTable.h"
+#include "Disk_Class/Disk.h"
+#include "FrontendInterface/FrontendInterface.h"
+
+int main(int argc, char *argv[]) {
+  /* Initialize the Run Copy of Disk */
+  Disk disk_run;
+  // StaticBuffer buffer;
+  // OpenRelTable cache;
+
+  return FrontendInterface::handleFrontend(argc, argv);
+}
